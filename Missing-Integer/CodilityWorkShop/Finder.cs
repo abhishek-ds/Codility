04bbb479bfecfb7cdcd6070a19a67fc5e631b52f
Abhishek DS <abhishekds1992@gmail.com>
2016-06-23 10:54:44 +0530

    Adding Sol

---
+++ Missing-Integer/CodilityWorkShop/Finder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodilityWorkShop
+{
+    class Finder
+    {
+       public void Solution(int[] intArray, int arrayLength)
+        {
+            Array.Sort(intArray);
+            int i = 1;
+           
+            while (i< arrayLength)
+           {
+               if (((IList<int>)intArray).Contains(i))
+               {
+                   i++;
+               }
+
+               else
+               {
+                    Console.WriteLine(i);
+                    i = arrayLength;
+               }
+           }
+           Console.ReadLine();
+        }
+    }
+}
---
+++ Missing-Integer/CodilityWorkShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CodilityWorkShop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Finder find = new Finder();
+            int[] arr = {-1, 6, 3, 1, 2, 5};
+            find.Solution(arr, arr.Length);
+
+        }
+    }
+}
